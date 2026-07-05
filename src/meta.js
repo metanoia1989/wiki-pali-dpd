cc65c978ff4f18c3d373fe8f00ba11256672619b
Adam Smith <sogaxili@gmail.com>
2026-07-05 21:41:29 +0800

    fix: 修整脚本下载tag

--- src/meta.js
+++ src/meta.js
@@ -22,7 +22,7 @@ export default `// ==UserScript==
 // @match        http://127.0.0.1:8080/*
 // @match        http://localhost:8080/*
 // @icon         https://pali-declension.mysticalpower.uk/favicon.svg
-// @@downloadURL https://pali-declension.mysticalpower.uk/wiki-pali-dpd.user.js
+// @downloadURL https://pali-declension.mysticalpower.uk/wiki-pali-dpd.user.js
 // @homepageURL  https://pali-declension.mysticalpower.uk/
 // @require      https://cdn.jsdelivr.net/npm/sql.js@1.11.0/dist/sql-wasm.js
 // @require      https://cdn.jsdelivr.net/npm/pako@2.1.0/dist/pako.min.js
